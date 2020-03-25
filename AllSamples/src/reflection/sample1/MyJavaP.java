efb451dc90fc8cf3e12d48f5b6f380d04b0788c6
Yusuf Andre TOKER <yusufandre@outlook.com>
2020-03-25 09:55:59 +0300

    Reflection First Sample
    Display properties, constructors and methods info for the given class as input

---
+++ AllSamples/src/reflection/sample1/MyJavaP.java
@@ -0,0 +1,37 @@
+package reflection.sample1;
+
+import java.lang.reflect.*;
+
+public class MyJavaP {
+
+    public static void main(String[] args) throws Exception {
+
+        Class cls;
+
+        cls = Class.forName(args[0]);
+
+        System.out.println("Fields");
+
+        Field[] fields = cls.getDeclaredFields();
+
+        for (Field field : fields) {
+            System.out.println(field);
+        }
+
+        System.out.println("Constructors");
+
+        var ctrs = cls.getConstructors();
+
+        for (var ctr : ctrs) {
+            System.out.println(ctr);
+        }
+
+        System.out.println("Methods");
+
+        Method[] methods = cls.getMethods();
+
+        for (Method method : methods) {
+            System.out.println(method);
+        }
+    }
+}
